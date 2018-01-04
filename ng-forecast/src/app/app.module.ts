6e736ff930b2d479241ba1c8c10dc16433673e9a
howdy39 <tatsuya.howdy@gmail.com>
2018-01-04 10:48:11 +0900

    コンポーネントを追加

--- ng-forecast/src/app/app.module.ts
+++ ng-forecast/src/app/app.module.ts
@@ -4,6 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressBarModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from "@angular/material";
 
 
 @NgModule({
@@ -12,7 +24,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
