766fe6b4b41d14db006d88bff9d441ccf773178f
howdy39 <tatsuya.howdy@gmail.com>
2018-01-04 21:07:51 +0900

    カスタムパイプによる日付形式の変換

--- ng-forecast/src/app/pipes/unix-time-date.pipe.ts
+++ ng-forecast/src/app/pipes/unix-time-date.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'unixTimeDate'
 })
 export class UnixTimeDatePipe implements PipeTransform {
-
+  private datePipe = new DatePipe('ja-JP');
   transform(value: any, args?: any): any {
-    return null;
+    // UnixTimeから戻しつつDatePipeを実行
+    return this.datePipe.transform(new Date(value * 1000), args);
   }
-
 }
