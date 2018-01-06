4e958d1e7f20b84db649b3d378a5002cafeb0427
howdy39 <tatsuya.howdy@gmail.com>
2018-01-06 22:25:09 +0900

    リストをアニメーション表示

--- ng-forecast/src/app/forecast/forecast.component.ts
+++ ng-forecast/src/app/forecast/forecast.component.ts
@@ -4,11 +4,24 @@ import { OpenWeatherMapService } from '../services/open-weather-map.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
 import { OpenWeatherMap } from '../shared/models/open-weather-map';
+import { slideFadeIn } from '../app.animations';
+import { query, stagger, transition, trigger, useAnimation } from '@angular/animations';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.scss']
+  styleUrls: ['./forecast.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      transition(':enter', [
+        query('mat-list-item', [
+          stagger(100, [
+            useAnimation(slideFadeIn)
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class ForecastComponent implements OnInit {
   public currentWeatherObservable: Observable<OpenWeatherMap.Current>;
